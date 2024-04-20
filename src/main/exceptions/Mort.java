0bf1674c976440a7b36541b5bc9a1b5aac43371f
Clément Soulier <clement.soulier12@gmail.com>
2024-04-20 11:07:46 +0200

    amélioration des exceptions

--- src/main/exceptions/Mort.java
+++ src/main/exceptions/Mort.java
@@ -1,11 +1,18 @@
 package main.exceptions;
 
 public class Mort extends Exception {
+    private int numero_citoyen;
+
     public Mort() {
         super("Cette personne est déjà marié(e)");
     }
 
     public Mort(int num_citoyen) {
-        super("Le citoyen ayant le numéro" + " " + num_citoyen + " " + "est mort");
+        super("Le citoyen ayant le numéro" + " " + num_citoyen + " " + "est mort(e)");
+        numero_citoyen = num_citoyen;
+    }
+
+    public String to_string() {
+        return "Le citoyen ayant le numéro" + " " + numero_citoyen + " " + "est mort(e)";
     }
 }
